c91271457bd7e4965581a4ec4b964f9c7105fea3
Fabio Camacho <fabiocamacho14@hotmail.com>
2024-01-29 20:45:58 -0300

    add project

---
+++ src/main/java/com/oranet/learnthymeleaf/LearnthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.oranet.learnthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnthymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oranet/learnthymeleaf/LearnthymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oranet.learnthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnthymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
